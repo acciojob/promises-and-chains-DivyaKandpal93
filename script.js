ab1564cb67cdc96af9f457fad6cac3ede887bb36
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-03-11 23:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
- function validateForm(event) {
-            event.preventDefault(); // Prevents form submission
-            
-            let age = document.getElementById("age").value.trim();
-            let name = document.getElementById("name").value.trim();
-            
-            if (name === "" || age === "") {
-                alert("Please enter valid details.");
-                return;
-            }
-            
-            let ageNumber = parseInt(age);
-            
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (ageNumber >= 18) {
-                        resolve(`Welcome, ${name}. You can vote.`);
-                    } else {
-                        reject(`Oh sorry, ${name}. You aren't old enough.`);
-                    }
-                }, 4000);
-            })
-            .then(message => alert(message))
-            .catch(error => alert(error));
+function validationForm(event) {
+        event.preventDefault();
+        let name = document.getElementById("nameValue").value.trim();
+        let age = document.getElementById("ageValue").value.trim();
+        if (name === "" || age === "") {
+          alert("Please enter valid details.");
+          return;
         }
+        let ageNumber = parseInt(age);
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (ageNumber >= 18) {
+              resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+              reject(`Oh sorry, ${name}. You aren't old enough.`);
+            }
+          }, 4000);
+        }).then(result => alert(result))
+        .catch(error => alert(error));
+      }
