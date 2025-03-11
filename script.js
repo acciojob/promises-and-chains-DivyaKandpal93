d378f30ca4341938a136cc46f58278bc22432e11
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-03-11 21:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+ function validateForm(event) {
+            event.preventDefault(); // Prevents form submission
+            
+            let age = document.getElementById("age").value.trim();
+            let name = document.getElementById("name").value.trim();
+            
+            if (name === "" || age === "") {
+                alert("Please enter valid details.");
+                return;
+            }
+            
+            let ageNumber = parseInt(age);
+            
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    if (ageNumber >= 18) {
+                        resolve(`Welcome, ${name}. You can vote.`);
+                    } else {
+                        reject(`Oh sorry, ${name}. You aren't old enough.`);
+                    }
+                }, 4000);
+            })
+            .then(message => alert(message))
+            .catch(error => alert(error));
+        }
